dd60c198e868925835fb2433cd88152fb6ef3538
davidhennig <davidjhennig@gmail.com>
2020-04-16 18:31:19 -0400

    thursday

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,9 +2,21 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+def rock_paper_scissors(n, cache=None):
 
+  inputs = [['rock'], ['paper'], ['scissors']] 
+  results = []
+
+  if n == 0:
+    return []
+  if n == 1:
+    return inputs
+  else:
+    for i in inputs:
+      for j in inputs:
+        results.append(i * n)
+      print("This is i", i)
+  return results
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
@@ -12,3 +24,4 @@ if __name__ == "__main__":
     print(rock_paper_scissors(num_plays))
   else:
     print('Usage: rps.py [num_plays]')
+  
